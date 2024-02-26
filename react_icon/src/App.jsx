e1aba70e9c8997b14d4efaf75ec2dad00daff1b7
Akash <divateakash21@gmail.com>
2024-02-26 15:29:02 +0530

    update

---
+++ react_icon/src/App.jsx
@@ -0,0 +1,19 @@
+import { IoMdAdd } from "react-icons/io";
+import { MdDelete } from "react-icons/md";
+
+
+
+function App() {
+ 
+  return (
+    <>
+    //To add icons , go to the react icons github pages copy the global command for installing the react icon dependence That is npm install react icon --save .
+    // after that select the icon for adding , use itsname the component in jsx file or where you want find .
+    import it add last letter as its first name .
+    <IoMdAdd />
+    <MdDelete />// Delete icons
+    </>
+  )
+}
+
+export default App
